5ba395ac6c6f69c713cf1d3c89337108ac2c69f1
OrkunSA <63612622+OrkunSA@users.noreply.github.com>
2020-06-02 15:41:19 -0500

    Update cli.rb

--- lib/breaking_bad_cli/cli.rb
+++ lib/breaking_bad_cli/cli.rb
@@ -1,46 +1,98 @@
 class Cli
-
-    attr_accessor :name
-
+    #CLI starts
     def call
-        system("clear") 
-        puts " -------------------------------------------------------------------- "
-        puts "|                                                                    |"
-        puts '|       Welcome to the best TV show of all time. Breaking Bad        |'
-        puts '|                 "I am not in danger. I am the danger."             |'
-        puts "|                                         - Walter White             |"
-        puts " -------------------------------------------------------------------- "
-        sleep(1.5)
-        load_data
-        main
+        system("clear") #clears terminal
+        logo #This loads the ASCII Art data I created to welcome users.
+        sleep(0.5)
+        load_data #That's how I get the data for quotes and characters from API
+        print_main_menu
     end
 
-    def load_data 
-        puts "Loading data now...".cyan
-        data_loader = Api.get_character
-        sleep(1.5)
-        puts "Data loaded. Here we go!".cyan
+    def print_main_menu #Main Menu
+        puts "To view all of the characters from the show, type 'list characters'".cyan
+        puts "To view all of the quotes from the show, type 'list quotes'".cyan
+        puts "To leave, type exit".cyan
+
+        main_menu_selection
     end
 
-    def main
-        print_all
+    def main_menu_selection #User makes his choice from the main menu. 
+        input = gets.chomp
+
+        if input.downcase == "list characters"
+            list_characters
+            sub_menu #That's where we go one level deep
+        elsif input.downcase == "list quotes"
+            list_quotes
+            print_continue
+            continue?(prompt_selection)
+        elsif input.downcase == "exit"
+            exit_cli
+        else
+            print_error
+            print_main_menu
+        end
+    end
+
+    def sub_menu
         print_selection_prompt
-        id = valid_id?(prompt_selection)
-        updated_character = get_character_details(id)
-        print_details(updated_character)
+        id = valid_id?(prompt_selection)#This method takes the input and checks if it's between 1 and Character.all.size
+        character = Character.find_by_id(id)# This method locates that specific character in the array.
+        print_details(character)
         print_continue
-        continue?
+        continue?(prompt_selection)
+        print_main_menu 
+        main_menu_selection
     end
-        
-
-    def print_all
+    
+    def list_characters
         Character.all.each.with_index(1) {|c, i| puts "#{i}.) #{c.name}".light_magenta}
     end 
 
-    def print_error
-        puts "Invalid Selection, please try again".cyan
+    def list_quotes
+        Quote.all.each.with_index(1) {|q, i| puts "#{i}.) #{q.quote} - #{q.author}".light_red}
     end
 
+    def logo
+        puts"
+        --  ██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗    ████████╗ ██████╗ 
+        --  ██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝    ╚══██╔══╝██╔═══██╗
+        --  ██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗         ██║   ██║   ██║
+        --  ██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝         ██║   ██║   ██║
+        --  ╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗       ██║   ╚██████╔╝
+        --   ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝       ╚═╝    ╚═════╝ 
+
+
+        --  ██████╗ ██████╗ ███████╗ █████╗ ██╗  ██╗██╗███╗   ██╗ ██████╗     ██████╗  █████╗ ██████╗
+        --  ██╔══██╗██╔══██╗██╔════╝██╔══██╗██║ ██╔╝██║████╗  ██║██╔════╝     ██╔══██╗██╔══██╗██╔══██╗
+        --  ██████╔╝██████╔╝█████╗  ███████║█████╔╝ ██║██╔██╗ ██║██║  ███╗    ██████╔╝███████║██║  ██║
+        --  ██╔══██╗██╔══██╗██╔══╝  ██╔══██║██╔═██╗ ██║██║╚██╗██║██║   ██║    ██╔══██╗██╔══██║██║  ██║
+        --  ██████╔╝██║  ██║███████╗██║  ██║██║  ██╗██║██║ ╚████║╚██████╔╝    ██████╔╝██║  ██║██████╔╝
+        --  ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝     ╚═════╝ ╚═╝  ╚═╝╚═════╝
+        
+        --   ██████╗██╗     ██╗
+        --   ██╔════╝██║     ██║
+        --   ██║     ██║     ██║
+        --   ██║     ██║     ██║
+        --   ╚██████╗███████╗██║
+        --    ╚═════╝╚══════╝╚═╝ ".light_red
+    end 
+
+    def load_data 
+        puts "Loading data now...".cyan
+        sleep(1.5)
+        character_loader = Api.get_character
+        quotes_loadder = Api.get_quotes
+        puts "Done!".cyan
+        sleep(0.5)
+    end
+
+        
+    def print_error
+        puts "Invalid Selection, please try again".light_red
+    end
+    
+      #Calls the instance methods in Character class and displays the information based on that character.
     def print_details(character)
         puts "Name:  #{character.name}".cyan
         puts "Birthday:  #{character.birthday}".cyan
@@ -61,10 +113,12 @@ class Cli
 
     def exit_cli
         puts "Thanks for using our CLI!".cyan
-        puts "Have a great day".cyan
+        puts "Have a great day!!!!".cyan
+        exit
     end
 
-
+    #"gets" is a method that asks the user to input something.
+    #"chomp" is a method that removes the blank line that is automatically created by "gets" after the input.
     def prompt_selection
         gets.chomp
     end 
@@ -74,22 +128,19 @@ class Cli
         if id < 1 || id > Character.all.size
             print_error
             sleep(1) 
-            main
+            main_menu_selectio
         end
         id
     end
 
     def continue?(choice)
         if choice == "y"
-            main
+            print_main_menu
+            main_menu_selection
         else
             exit_cli
+            exit
         end
-    end
-
-
-    def get_character_details(id)
-        Api.get_details_by_id(id)
     end
 
 end
